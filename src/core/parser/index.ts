a2928aa3f21591ab298875a2990b6c2022a84e73
José Martínez Santana <jose.martinez.santana@gmail.com>
2025-10-25 23:24:29 -0600

    feat(parser): add NestedObjectParser for handling nested object data

--- src/core/parser/index.ts
+++ src/core/parser/index.ts
@@ -1,3 +1,4 @@
 export { createArrayParser } from "./array-parser.mts";
 export { createObjectParser } from "./object-parser.mts";
 export { createObjectOfArraysParser } from "./object-of-arrays-parser.mts";
+export { createNestedObjectParser } from "./nested-object-parser.mts";
